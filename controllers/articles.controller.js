824721fd14c5bc2ba6a34699432163ff88a5eead
Verity Gregory <verity.gregory@gmail.com>
2023-08-21 14:39:07 +0100

    add get all articles end point with comment count in date order

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,10 +1,19 @@
-const { fetchArticle } = require("../models/articles.models");
+const { fetchArticle, fetchAllArticles } = require("../models/articles.models");
 
 exports.getArticleById = async (req, res, next) => {
   try {
     const { article_id } = req.params;
     const article = await fetchArticle(article_id);
     res.status(200).send({ article });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getAllArticles = async (req, res, next) => {
+  try {
+    const articles = await fetchAllArticles();
+    res.status(200).send({ articles });
   } catch (err) {
     next(err);
   }
