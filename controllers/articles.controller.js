158c63be2186695737250ef57168fdb554cdb7e5
Verity Gregory <verity.gregory@gmail.com>
2023-08-21 16:48:11 +0100

    get all comments by id with error handling

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,4 +1,8 @@
-const { fetchArticle, fetchAllArticles } = require("../models/articles.models");
+const {
+  fetchArticle,
+  fetchAllArticles,
+  fetchCommentsById,
+} = require("../models/articles.models");
 
 exports.getArticleById = async (req, res, next) => {
   try {
@@ -14,6 +18,16 @@ exports.getAllArticles = async (req, res, next) => {
   try {
     const articles = await fetchAllArticles();
     res.status(200).send({ articles });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getCommentsById = async (req, res, next) => {
+  try {
+    const { article_id } = req.params;
+    const comments = await fetchCommentsById(article_id);
+    res.status(200).send({ comments });
   } catch (err) {
     next(err);
   }
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,4 +1,5 @@
 const db = require("../db/connection");
+const { checkExists } = require("../db/seeds/utils");
 
 exports.fetchArticle = async (id) => {
   const { rows } = await db.query(
@@ -17,6 +18,15 @@ exports.fetchAllArticles = async () => {
     `select a.article_id, a.title, a.topic, a.author, a.created_at, a.votes, a.article_img_url, count(c.comment_id)::int as comment_count from articles a
     left join comments c on a.article_id = c.article_id group by a.article_id order by a.created_at desc`
   );
-  console.log(rows);
+
+  return rows;
+};
+
+exports.fetchCommentsById = async (id) => {
+  const queryStr = `select * from comments where article_id = $1 order by created_at desc`;
+  const [_, { rows }] = await Promise.all([
+    checkExists("articles", "article_id", id),
+    db.query(queryStr, [id]),
+  ]);
   return rows;
 };
